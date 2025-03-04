ce74bac0aca09e9f0dd14236a1139db862cff211
Dominik Rybczyński <dominik.revalis@gmail.com>
2025-03-04 17:21:10 +0100

    Add Animated Square component, add retrieving settings from external method

---
+++ OverflowingPalette.Shared/Models/AnimatedSquare.cs
@@ -0,0 +1,9 @@
+﻿namespace OverflowingPalette.Shared.Models
+{
+    public class AnimatedSquare
+    {
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public string Color { get; set; }
+    }
+}
--- OverflowingPalette.WebApp/Services/GameService.cs
+++ OverflowingPalette.WebApp/Services/GameService.cs
@@ -1,4 +1,6 @@
-﻿namespace OverflowingPalette.WebApp.Services
+﻿using OverflowingPalette.Shared.Models;
+
+namespace OverflowingPalette.WebApp.Services
 {
     public class GameService(HttpClient httpClient)
     {
@@ -8,6 +10,22 @@
             var results = await httpClient.GetFromJsonAsync<IEnumerable<string>>(uri);
 
             return results!;
+        }
+
+        public IEnumerable<AnimatedSquare> GetAnimatedSquares()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    yield return new AnimatedSquare
+                    {
+                        PositionX = i,
+                        PositionY = j,
+                        Color = string.Empty,
+                    };
+                }
+            }
         }
     }
 }
